487cfe6636d143111948db87814b19d1df622012
dgr-aiwe <dgr@aiweapps.com>
2020-08-23 16:27:07 +0300

    modified dependecies, set activity layout

--- app/src/main/java/ru/groshevdg/ApplicationActivity.kt
+++ app/src/main/java/ru/groshevdg/ApplicationActivity.kt
@@ -3,7 +3,7 @@ package ru.groshevdg
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ApplicationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
